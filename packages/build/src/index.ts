567807bb46675bbcc7c5145ef3bf610604140d66
tnraro <deokji53@gmail.com>
2023-08-25 12:17:10 +0900

    feat: add build package

---
+++ packages/build/src/index.ts
@@ -0,0 +1,19 @@
+import { dirname, join } from "path";
+
+const __dirname = dirname(new URL(import.meta.url).pathname);
+const __root = join(__dirname, "../../ui/dist/assets");
+const __output = join(__dirname, "../pickle.user.js");
+
+const js: string = await Bun.file(join(__root, "index.js")).text();
+const css: string = await Bun.file(join(__root, "index.css")).text();
+const template: string = await Bun.file(join(__dirname, "userscript-template.js")).text();
+
+const userscript = [
+  template,
+  `GM_addStyle(\`${css}\`);`,
+  js,
+].join("\n");
+
+console.log(__output);
+
+Bun.write(__output, userscript);
---
+++ packages/build/src/userscript-template.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name   pickle
+// @match  https://picrew.me/*/image_maker/*
+// @icon   https://www.google.com/s2/favicons?sz=64&domain=picrew.me
+// @grant  GM_addStyle
+// @run-at document-start
+// ==/UserScript==
+const root = document.createElement("div");
+root.setAttribute("id", "pickle-app");
+root.style = "position: absolute; top: 0; left: 0;";
+document.body.appendChild(root);
